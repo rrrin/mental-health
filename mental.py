fcb814ffaa0f941415746fde6d887b93aaf729a9
rrrin <kate.voropaeva@gmail.com>
2023-12-28 23:35:07 +0700

    правки

--- mental.py
+++ mental.py
@@ -7,12 +7,31 @@ Created on Mon Nov 27 17:34:50 2023
 
 import pandas as pd
 import numpy as np
+class MoneyBox:
+    def __init__(self, capacity):
+        self.capacity=capacity
+        self.amount=0 # конструктор с аргументом – вместимость копилки
 
-# file0=open('data.csv', 'r')
-# head=file0.readline()
+    def can_add(self, v):
+        if self.amount+v<=self.capacity: return True
+        else: return False
+        # True, если можно добавить v монет, False иначе
 
-# Data=pd.DataFrame()
+    def add(self, v):
+        self.amount+=v
+        # положить v монет в копилку
+ 
+        
+ 
+import time
 
-
-file1=pd.read_csv('data.csv', sep='\t')
-file1=file1.dropna()
+class Loggable:
+    def log(self, msg):
+        print(str(time.ctime()) + ": " + str(msg))
+class LoggableList(Loggable,list):
+    def append(self, x):
+        super(LoggableList, self).append(x)
+        self.log(x)
+        
+a=LoggableList()
+a.append('1')

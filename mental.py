e553f5e3463d45a7c5540a96639e6c552dbe6889
rrrin <kate.voropaeva@gmail.com>
2024-01-08 20:54:04 +0700

    Оформление финал
    Добавлено описание шагов, функций, перечень задач, удалены лишние графики

--- mental.py
+++ mental.py
@@ -23,15 +23,84 @@ class MoneyBox:
  
         
  
-import time
+    
+class multifilter:
+    def judge_half(pos, neg):
+        return pos>=neg
+            
+        # допускает элемент, если его допускает хотя бы половина фукнций (pos >= neg)
 
-class Loggable:
-    def log(self, msg):
-        print(str(time.ctime()) + ": " + str(msg))
-class LoggableList(Loggable,list):
-    def append(self, x):
-        super(LoggableList, self).append(x)
-        self.log(x)
+    def judge_any(pos, neg):
+        # допускает элемент, если его допускает хотя бы одна функция (pos >= 1)
+        return pos>=1
+
+    def judge_all(pos, neg):
+        # допускает элемент, если его допускают все функции (neg == 0)
+        return neg==0
+
+    def __init__(self, iterable, *funcs, judge=judge_any):
+        self.iterable=iterable
+        new_iter=[]
+        pos=0
+        neg=0
+        for f in funcs:
+            
+        # iterable - исходная последовательность
+        # funcs - допускающие функции
+        # judge - решающая функция
+
+    def __iter__(self):
+        return myiter(self)
+        # возвращает итератор по результирующей последовательности
         
-a=LoggableList()
-a.append('1')
+class myiter:
+    def __init__(self, elements):
+        self.el=elements
+        self.length=len(elements)
+        self.i=0
+    def __next__(self):
+        if self.i<self.length:
+            self.i+=1
+            return self.el[self.i]
+        else:
+            raise StopIteration
+            
+
+# def check_parent(c1,c2):
+#     if c1==c2: 
+#         return 1
+#     if c1 in exceptions[c2]: return 1
+#     if exceptions[c2]=='None': return 0
+#     s=0
+#     for x in exceptions[c2]:
+#         s+=check_parent(c1,x)
+#     return s
+
+# class NonPositiveError(Exceptions):
+#     pass
+# class PositiveList(list):
+#     def append(self,x):
+#         if(x>0):
+#             super(PositiveList, self).append(x)
+#         else:
+#             raise NonPositiveError
+
+# n=int(input())
+# exceptions={}
+# for i in range(n):
+#     data=input().split()
+#     if len(data)==1: exceptions[data[0]]=[]
+#     else: exceptions[data[0]]=data[2:]
+
+# m=int(input())
+# check_excep=dict.fromkeys(list(exceptions.keys()),0)
+# print(exceptions)
+# arr=[]
+# for i in range(m):
+#     ex=input()
+#     check_excep[ex]+=1
+#     for x in arr:
+#         if check_parent(x, ex)>0:
+#             print(ex)
+#             break
+#     arr.append(ex)
